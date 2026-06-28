64e33f731fd23c331f55e1275033b9233467edbf
Akash Yadav <ay2484900@gmail.com>
2026-06-28 23:33:23 +0530

    Add GitHub Actions CI/CD pipeline

---
+++ src/main/java/com/findetect/findetectai/FindetectAiApplication.java
@@ -0,0 +1,13 @@
+package com.findetect.findetectai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindetectAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FindetectAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/findetect/findetectai/FindetectAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.findetect.findetectai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindetectAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
